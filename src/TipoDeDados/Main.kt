52b697c2b18008d1b23e078502db765fe0d6706c
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-09-07 00:30:07 -0300

    Atualização

---
+++ src/TipoDeDados/Main.kt
@@ -0,0 +1,23 @@
+package TipoDeDados
+
+fun main() {
+
+    //Tipos de Dados Variáveis
+    val nomeCompleto: String = "Leonardo Coimbra de Paiva"
+    val idade: Int = 30
+    val salario: Double = 3500.0
+    val ativo: Boolean = true
+    val categoria: Char = 'A'
+
+    println("Nome: $nomeCompleto")
+    println("Idade: $idade")
+    println("Salario: $salario")
+    println("ativo: $ativo")
+    println("Categoria: $categoria")
+
+
+
+
+
+}
+
