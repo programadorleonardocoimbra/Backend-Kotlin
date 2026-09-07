2af403e83c2591cb2a05a76e5ceaedf6a38c119c
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-09-07 00:09:11 -0300

    Atualização

---
+++ src/MainPrintln/Main.kt
@@ -0,0 +1,15 @@
+package MainPrintln
+
+    fun main() {
+
+    // Podemos colocar uma Frase
+    println("Sistema Iniciado com Sucesso!!!")
+
+    // Podemos colocar os números somente
+    println(2026)
+
+    // Podemos realizar operações
+    println(10+5)
+
+    }
+
